967df26c8ee17fd2bce715d7e1625fca6656bf7d
kiran kumar <gonipallikirann1997@gmail.com>
2023-04-03 13:38:59 +0530

    1 commit

---
+++ src/test/java/com/MavenPractice/TS01.java
@@ -0,0 +1,12 @@
+package com.MavenPractice;
+
+import org.testng.annotations.Test;
+
+public class TS01 {
+	@Test
+	public void testMethod()
+	{
+		System.out.println("-----first test execute-----");
+	}
+
+}
---
+++ src/test/java/com/MavenPractice/TS02.java
@@ -0,0 +1,16 @@
+package com.MavenPractice;
+
+import org.testng.annotations.Test;
+
+public class TS02 {
+	@Test
+	public void testMethod()
+	{
+		System.out.println("-----first test execute-----");
+	}
+	@Test
+	public void kiran()
+	{
+		System.out.println("----second method execute-----");
+	}
+}
