cdadcad5c1e4fe0c8bafa7104c738b82f40ac92c
Manjunath Yadav <Manjunath Yadav@Manju12Yadav>
2023-04-03 17:11:40 +0530

    food commit

---
+++ src/test/java/com/MavenPractice/Foods.java
@@ -0,0 +1,17 @@
+package com.MavenPractice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Foods 
+{
+	@Test
+	public void menuList()
+	{
+		
+		Reporter.log("Masala Dosa",true);
+		
+		System.out.println("Mushroom");
+		
+	}
+}
